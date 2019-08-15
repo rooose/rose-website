1d84a50a692e09bf502cf0905f3e04f7aff1efe9
Ro se <rosehirigoyen@gmail.com>
2019-08-15 14:48:24 -0400

    Updates

---
+++ precache-manifest.7b97b37afccfd9136ba1a127a5f27589.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308d65ff986f0e6d3f65957c1f16d9ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9937f72662507b8887",
+    "url": "/static/css/main.9ba94510.chunk.css"
+  },
+  {
+    "revision": "21e930b7e28cdf1768e7",
+    "url": "/static/js/2.e7eae10e.chunk.js"
+  },
+  {
+    "revision": "6f9937f72662507b8887",
+    "url": "/static/js/main.9fa50d51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bbe12aefb3ed3e7304dadc3e984e7903",
+    "url": "/static/media/Profile.bbe12aef.pdf"
+  },
+  {
+    "revision": "ca80619125935a11c489048fbfe95ee8",
+    "url": "/static/media/flare.ca806191.png"
+  },
+  {
+    "revision": "51f31b036488f0a290019f27f0d5092c",
+    "url": "/static/media/heightMap.51f31b03.png"
+  },
+  {
+    "revision": "a4e3dfc9a4c17d69cfd609ed0a07f117",
+    "url": "/static/media/rose.a4e3dfc9.jpeg"
+  },
+  {
+    "revision": "eec03e7d641a97ea492f31110178e2d3",
+    "url": "/static/media/tiger_walk.eec03e7d.ttf"
+  }
+]);
